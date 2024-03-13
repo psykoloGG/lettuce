b00510fbb3318d8b6a6722fc50f49d0d473a3d65
PsykoloGG <lukas.kundelis@gmail.com>
2024-03-13 13:03:37 +0000

    Add factories and seeders
    to Comments, Posts, Users, Groups
    added example seeding of 10 users

--- database/migrations/2024_03_13_000521_create_comments_table.php
+++ database/migrations/2024_03_13_000521_create_comments_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string("content");
+            
         });
     }
 
